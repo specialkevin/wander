1c00010551cd5bc3ea94518dbff34a7c43596d17
Kevin Harriss <special.kevin@gmail.com>
2012-07-31 13:18:20 -0500

    Updated it to put any leftover values that are in someones contacts into the notes field.

--- wander/google.py
+++ wander/google.py
@@ -1,3 +1,4 @@
+import atom.data
 import gdata.contacts.data
 import gdata.contacts.client
 
@@ -43,30 +44,43 @@ class Contacts(object):
                     given_name = gdata.data.GivenName(text=firstName),
                     family_name = gdata.data.FamilyName(text=lastName),
                     full_name = gdata.data.FullName(text=full_name))
+                del contact['firstName']
+                del contact['lastName']
+                if 'middleName' in contact:
+                    del contact['middleName']
+                del contact['fullName']
 
                 if contact['email'] != '':
                     new_contact.email.append(gdata.data.Email(address = contact['email'], 
                         primary = 'true', display_name = full_name, rel = gdata.data.OTHER_REL))
+                    del contact['email']
                 if 'email2' in contact and contact['email2'] != '':
                     new_contact.email.append(gdata.data.Email(address = contact['email2'], 
                         display_name = full_name, rel = gdata.data.OTHER_REL))
+                    del contact['email2']
 
                 if 'homePhone' in contact and contact['homePhone'] != '':
                     new_contact.phone_number.append(gdata.data.PhoneNumber(text = contact['homePhone'], 
                         rel = gdata.data.HOME_REL))
+                    del contact['homePhone']
 
                 if 'workPhone' in contact and contact['workPhone'] != '':
                     new_contact.phone_number.append(gdata.data.PhoneNumber(text = contact['workPhone'], 
                         rel = gdata.data.WORK_REL))
+                    del contact['workPhone']
 
                 if 'mobilePhone' in contact and contact['mobilePhone'] != '':
                     new_contact.phone_number.append(gdata.data.PhoneNumber(text = contact['mobilePhone'],
                         rel = gdata.data.MOBILE_REL))
+                    del contact['mobilePhone']
 
                 if 'company' in contact and contact['company'] != '':
                     new_contact.organization = gdata.data.Organization(
                         org_name = gdata.data.OrgName(text = contact['company']),
                         rel = gdata.data.WORK_REL)
+                    del contact['company']
+
+                new_contact.content = atom.data.Content(text = '\n'.join(contact.values()))
     
                 self.gd_client.CreateContact(new_contact)
         return
